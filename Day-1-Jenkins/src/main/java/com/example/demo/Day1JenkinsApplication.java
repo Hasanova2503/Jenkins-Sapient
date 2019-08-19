faf59e510088b43839df87b5eb562b4743e15248
Hasan Shahid <hasan.shahid@publicissapient.com>
2019-08-19 13:10:58 +0530

    Jenkins

---
+++ Day-1-Jenkins/src/main/java/com/example/demo/Day1JenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1JenkinsApplication.class, args);
+	}
+
+}
